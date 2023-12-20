ebb92c9ab60b3ecc4751937ee816be6e8b90c726
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-12-20 18:26:37 +1300

    Brad is cool

--- CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
+++ CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb785ec7c05a03fbc30ffa4324c8a7023c299ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45b7d501b057f85ef83baf4a1fb35035dec84ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
